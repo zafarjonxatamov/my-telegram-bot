2b8ce01ffb476435cda3b201bfb7302bfae17c52
zafarjonxatamov <zxatamovjm@gmail.com>
2026-07-27 11:48:04 +0500

    Add comments to config.py for card information
    Added comments for clarity on card details.

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "BOT_TOKEN_SHU_YERGA")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "1819574194"))
-CARD_NUMBER = os.getenv("CARD_NUMBER", "9860100127991279")
+
+# O'z karta raqamingiz va ismingizni shu yerga yozing:
+CARD_NUMBER = os.getenv("CARD_NUMBER", "9860100127991279") 
 CARD_HOLDER = os.getenv("CARD_HOLDER", "MUHAMMADJON NAZIRJONOV")
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "OPENAI_KEY_SHU_YERGA")
 CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY", "")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
